5e680ef3abd4cf7e64baddffc6421c8b97e351d5
Bhabananda Prusty <prustybhabananda@gmail.com>
2022-07-03 10:42:49 +0530

    initial commit

---
+++ UserFileRunner.java
@@ -0,0 +1,129 @@
+package FsdProject;
+import java.util.Scanner;
+public class UserFileRunner {
+	UserFileRunner Userfiles;
+
+	public static void main(String[] args) {
+	UserFileRunner userfilesrunner =new UserFileRunner();
+	userfilesrunner.MainMenu();
+
+
+	}
+
+	public void MainMenu() {
+		System.out.println("Main Menu");
+		Userfiles = new UserFileRunner();
+		this.Userfiles.createDirectory();
+
+	try (Scanner input = new Scanner(System.in)) {
+		int mainSelection;
+
+
+		System.out.println("*************************************SIMPLILEARN*********************************************");
+		System.out.println("**********************************FSD PHASE1 PROJECT**********************************************");
+		System.out.println("**********************************Bhabananda Prusty***********************************************************");
+		System.out.println();
+
+		 
+
+
+
+		
+		 
+		do {
+		 
+
+		System.out.println("Choose from the below options"); 
+		System.out.println("1.Display the File Names in Ascending Order");
+
+		System.out.println("2.Display User Interface Menu");
+		System.out.println("3.Exit");
+
+
+		mainSelection = input.nextInt();
+
+		switch(mainSelection) {
+		case 1:
+		Userfiles.listFilesinAscendingOrder();
+		 
+		break;
+		 
+		case 2:
+		System.out.println("Option 2 selected");
+		userInterfaceMenu();
+		break; case 3:
+		System.out.println("Thank You for using our Application");
+		System.exit(0);
+		 
+		break;
+
+		default:
+		System.out.println("Wrong option");
+		}
+		}while(mainSelection!=3);
+	}
+	}
+	private void createDirectory() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void listFilesinAscendingOrder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void userInterfaceMenu() {
+	try (Scanner input = new Scanner(System.in)) {
+		int selection;
+		do {
+		System.out.println("***************USER INTERFACE MENU**************");
+		System.out.println("Choose from the below options");
+		System.out.println("1.Add a user specified file to the application");
+		 
+		 
+		
+		 
+
+		System.out.println("2.Delete a user specified file from the application"); 
+		System.out.println("3.Search for a user specified file from the application"); 
+			System.out.println("4.Back to Main Menu");
+
+		selection = input.nextInt();
+		switch(selection) {
+		case 1:
+		this.Userfiles.createuserFile();
+		break;
+
+		case 2:
+		System.out.println("Option 2 Selected");
+		this.Userfiles.createuserFile(); break;
+
+		case 3:
+		System.out.println("Option 3 Selected");
+		this.Userfiles.searchFile(); break;
+
+		case 4:
+		MainMenu();
+
+		default:
+		System.out.println("Wrong Option Selected");
+		 
+
+		}
+		}while(selection!=4);
+	}
+	}
+
+	private void searchFile() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void createuserFile() {
+		// TODO Auto-generated method stub
+		
+	}
+	}
+
+
